f412ada6b30f905936486344ba68e84ab1afae47
Rhey Victor Macayran <ramacayran@usa.edu.ph>
2024-12-16 02:02:15 +0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- fonr awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     
+     <!-- cusrom css file link -->
+      <link rel="stylesheet" href="css/about.css">
+
+</head>
+<body>
+            <!-- header section starts -->
+            <section class="header" id="header">
+        <a href="home.php" class="logo">
+            <img src="images/Logo.png" alt=""></span>
+        </a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="portfolio.php">portfolio</a>
+            <a href="conatct.php">contact</a>
+            <a href="contact.php"><button>Book a Call <i class="fa-solid fa-arrow-up-right-dots"></i> </button></a>
+        </nav>
+
+
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+     </section>
+    <!-- header section ends -->
+    
+
+    <div class="container">
+    <div class="content">
+      <h1 class="main-heading">We love bringing your ideas, to life</h1>
+      <p class="description">
+        During the height of the global pandemic, our team was driven by a common goal: to provide affordable and high-quality software solutions to small businesses. As the situation evolved, our team expanded and grew, becoming a dependable digital partner for numerous local and international MSMEs (Micro, Small and Medium Enterprises) as well as startups.
+      </p>
+      <p class="description">
+        We take pride in being a youthful and dynamic team, fueled by our passion for developing innovative solutions. Our mission is to offer impactful and transformative solutions that propel business forward while simultaneously enhancing the lives of people through meaningful innovation. With an unwavering commitment to excellence, we strive to empower businesses and individuals alike, leveraging the potential of technology to drive growth and foster positive change.
+      </p>
+      <p class="signature">
+        <span class="name">Rhey Macayran</span><br>
+        <small>Founder and Chief Executive Officer</small>
+      </p>
+    </div>
+    <div class="image-container">
+      <img src="images/Rhey.png" alt="Rhey Macayran working on a laptop">
+    </div> 
+  </div>
+
+  <div class="container">
+      <div class="content">
+        <h1 class="main-heading">We love bringing your ideas, to life</h1>
+        <p class="description">
+          During the height of the global pandemic, our team was driven by a common goal: to provide affordable and high-quality software solutions to small businesses. As the situation evolved, our team expanded and grew, becoming a dependable digital partner for numerous local and international MSMEs (Micro, Small and Medium Enterprises) as well as startups.
+        </p>
+        <p class="description">
+          We take pride in being a youthful and dynamic team, fueled by our passion for developing innovative solutions. Our mission is to offer impactful and transformative solutions that propel business forward while simultaneously enhancing the lives of people through meaningful innovation. With an unwavering commitment to excellence, we strive to empower businesses and individuals alike, leveraging the potential of technology to drive growth and foster positive change.
+        </p>
+        <p class="signature">
+          <span class="name">Rhey Macayran</span><br>
+          <small>Founder and Chief Executive Officer</small>
+        </p>
+      </div>
+    <div class="image-container reverse">
+      <img src="images/Wendy.png" alt="Rhey Macayran working on a laptop">
+    </div> 
+  </div>
+
+  <div class="container">
+    <div class="content">
+      <h1 class="main-heading">We love bringing your ideas, to life</h1>
+      <p class="description">
+        During the height of the global pandemic, our team was driven by a common goal: to provide affordable and high-quality software solutions to small businesses. As the situation evolved, our team expanded and grew, becoming a dependable digital partner for numerous local and international MSMEs (Micro, Small and Medium Enterprises) as well as startups.
+      </p>
+      <p class="description">
+        We take pride in being a youthful and dynamic team, fueled by our passion for developing innovative solutions. Our mission is to offer impactful and transformative solutions that propel business forward while simultaneously enhancing the lives of people through meaningful innovation. With an unwavering commitment to excellence, we strive to empower businesses and individuals alike, leveraging the potential of technology to drive growth and foster positive change.
+      </p>
+      <p class="signature">
+        <span class="name">Rhey Macayran</span><br>
+        <small>Founder and Chief Executive Officer</small>
+      </p>
+    </div>
+    <div class="image-container">
+      <img src="images/Rod.png" alt="Rhey Macayran working on a laptop">
+    </div> 
+  </div>
+
+
+
+
+
+<!-- footer section starts -->
+
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">   
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="portfolio.php"> <i class="fas fa-angle-right"></i> portfolio</a>
+        <a href="conatct.php"> <i class="fas fa-angle-right"></i> contact</a>
+    </div>
+
+    <div class="box">   
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">   
+        <h3>contact information</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +xxx-xxx-xxxx</a>
+        <a href="#"> <i class="fas fa-phone"></i> +xxx-xxx-xxxx</a>
+        <a href="#"> <i class="fas fa-envelope"></i> galadevs@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i> IloIlo City, Philippines</a>
+    </div>
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+        <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+        <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+    </div>
+
+</div>
+
+<div class="credit"> created by <span>GalaDevs</span> | all rights reserved!</div>
+</section>
+
+
+ 
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+ <script src="js/script.js"></script>
+
+    
+</body>
+</html>
