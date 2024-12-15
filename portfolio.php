1051ca96d72d9f4932c4780cead336e1c54a98d4
Rhey Victor Macayran <ramacayran@usa.edu.ph>
2024-12-16 02:03:31 +0800

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- fonr awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     
+     <!-- cusrom css file link -->
+      <link rel="stylesheet" href="css/portfolio.css">
+
+</head>
+<body>
+            <!-- header section starts -->
+            <section class="header" id="header">
+        <a href="home.php" class="logo">
+            <img src="images/Logo.png" alt=""></span>
+        </a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="portfolio.php">portfolio</a>
+            <a href="conatct.php">contact</a>
+            <a href="contact.php"><button>Book a Call <i class="fa-solid fa-arrow-up-right-dots"></i> </button></a>
+        </nav>
+
+
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+     </section>
+
+     <div class="container">
+        <div class="container1">
+            <div class="left">
+            <h1 class="title">Case Studies</h1>
+
+        <div class="filter-buttons">
+            <button class="filter-btn active" data-category="all">All Projects</button>
+            <button class="filter-btn" data-category="web">Web Development</button>
+            <button class="filter-btn" data-category="design">Product Design</button>
+        </div>
+            </div>
+            <div class="right">
+            <p class="description">Showcasing our expertise in software development, website and mobile app development, and dedication to delivering exceptional results.</p>
+            </div>
+        
+        
+        </div>
+        
+
+
+        <div class="projects">
+            <div class="project-item" data-category="web">
+                <div class="container">
+                    <img src="images/portfolio 1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+            <div class="project-item" data-category="design">
+                <div class="container">
+                    <img src="images/product1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="project-item" data-category="web">
+            <div class="container">
+                <img src="images/portfolio 1.png" alt="Project Image">
+                <div class="project-card left">
+                    <h2>BMPH</h2>
+                    <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                    <button class="case-study-btn">Case Study ↗</button>
+                </div>
+            </div>
+        </div>
+            <div class="project-item" data-category="design">
+            <div class="container">
+                    <img src="images/product1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+            <div class="project-item" data-category="web">
+                <div class="container">
+                    <img src="images/portfolio 1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+            <div class="project-item" data-category="design">
+                <div class="container">
+                    <img src="images/product1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="project-item" data-category="web">
+            <div class="container">
+                <img src="images/portfolio 1.png" alt="Project Image">
+                <div class="project-card left">
+                    <h2>BMPH</h2>
+                    <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                    <button class="case-study-btn">Case Study ↗</button>
+                </div>
+            </div>
+        </div>
+            <div class="project-item" data-category="design">
+            <div class="container">
+                    <img src="images/product1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+            <div class="project-item" data-category="web">
+                <div class="container">
+                    <img src="images/portfolio 1.png" alt="Project Image">
+                    <div class="project-card left">
+                        <h2>BMPH</h2>
+                        <p>BMPH, the Philippines' top gaming community, bringing players together to explore the latest games, events, and esports action. Join us to connect, compete, and level up your gaming experience!</p>
+                        <button class="case-study-btn">Case Study ↗</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        </div>
+        
+    </div>
+
+
+
+
+    
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">   
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="portfolio.php"> <i class="fas fa-angle-right"></i> portfolio</a>
+        <a href="conatct.php"> <i class="fas fa-angle-right"></i> contact</a>
+    </div>
+
+    <div class="box">   
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">   
+        <h3>contact information</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +xxx-xxx-xxxx</a>
+        <a href="#"> <i class="fas fa-phone"></i> +xxx-xxx-xxxx</a>
+        <a href="#"> <i class="fas fa-envelope"></i> galadevs@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i> IloIlo City, Philippines</a>
+    </div>
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+        <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+        <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+    </div>
+
+</div>
+
+<div class="credit"> created by <span>GalaDevs</span> | all rights reserved!</div>
+</section>
+
+
+    
+     
+     <!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+ <script src="js/script.js"></script>
+ 
+
+    
+</body>
+</html>
